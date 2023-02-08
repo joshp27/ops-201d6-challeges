120116fa9762aee12ef0acbd4f8116255907a785
joshp27 <joshphipps2@aol.com>
2023-02-08 05:23:11 +0000

    finished hello world.sh

---
+++ helloworld.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 02 Ops Challenge Solution
+# Joshua Phipps
+# 2/7/2023
+# Purpose: Print a string to the terminal
+
+
+
+# Main
+helloworld (){
+    echo hello world
+    echo How are you?
+}
+wassup (){
+    echo yo $1
+}
+helloworld
+wassup joshua
+hola () {
+    echo papa $1
+}
+hola mami
+# End
