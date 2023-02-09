116238ca59dcc53f31dc82d1a186c84712a06d9e
joshp27 <joshphipps2@aol.com>
2023-02-09 02:29:15 +0000

    OPS challenged 03

---
+++ code_3.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 03 Ops Challenge Solution
+# Joshua Phipps
+# 2/8/2023
+# Purpose: Print the login history of users on this computer
+
+#Main
+
+hislog() {
+    last --fullnames
+}
+hislog
+#End
--- helloworld.sh
+++ helloworld.sh
@@ -22,3 +22,16 @@ hola () {
 }
 hola mami
 # End
+
+
+#!/bin/bash
+
+# Script: Ops 201 Class 03 Ops Challenge Solution
+# Joshua Phipps
+# 2/8/2023
+# Purpose: Print the login history of users on this computer
+
+#Main
+
+
+#End
