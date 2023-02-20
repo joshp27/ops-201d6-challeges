d3f30f51195daf3d9b42a0a16405705a9900d667
joshp27 <joshphipps2@aol.com>
2023-02-20 23:35:42 +0000

    This is code 11

---
+++ code11.ps1
@@ -0,0 +1,25 @@
+# Script: Ops 201 Class 11 Ops Challenge Solution
+# Joshua Phipps
+# 2/20/2023
+# Purpose: Automated Endpoint Configuration
+
+
+
+
+##Enable File and Printer Sharing
+Set-NetFirewallRule -DisplayGroup "File And Printer Sharing" -Enabled True
+
+##Allow ICMP traffic
+netsh advfirewall firewall add rule name="Allow incoming ping requests IPv4" dir=in action=allow protocol=icmpv4
+
+##Enable Remote management
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
+
+##Remove bloatware
+iex ((New-Object System.Net.WebClient).DownloadString('https://git.io/debloat'))
+
+##Enable Hyper-V
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
+
+##Disable SMBv1, an insecure protocol
+Set-SmbServerConfiguration -EnableSMB1Protocol $false -Force
